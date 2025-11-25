780b4910cbc2507a1fd87e2585ec25f26bc3128c
Jackson Lino <jackson.lino@hotmail.com>
2025-11-25 03:30:15 -0300

    feat: :sparkles: make routes balance and reset
    the routes balance and reset are now available to use. feature tests implemented.

---
+++ tests/Feature/BalanceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\Account;
+use App\Repositories\AccountRepository;
+use Illuminate\Support\Facades\Cache;
+
+beforeEach(closure: function (): void {
+    Cache::flush();
+});
+
+test(description: 'get balance for existing account', closure: function (): void {
+    $accountRepository = new AccountRepository();
+    $accountRepository->save(account: new Account(id: 'user_1', balance: 1000));
+
+    $this->get(uri: '/balance?account_id=user_1')
+        ->assertStatus(status: 200)
+        ->assertContent(value: '1000');
+});
+
+test(description: 'get balance for non-existing account returns 404', closure: function (): void {
+    $this->get(uri: '/balance?account_id=1234')
+        ->assertStatus(status: 404)
+        ->assertContent(value: '0');
+});
+
+test(description: 'reset accounts, clears all data successfully', closure: function (): void {
+    $this->post(uri: '/reset')
+        ->assertStatus(status: 200)
+        ->assertContent(value: 'OK');
+});
