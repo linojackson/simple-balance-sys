0741246abeea2fb4243edd6593b9e6157720adc3
Jackson Lino <jackson.lino@hotmail.com>
2025-11-25 01:16:27 -0300

    feat: :sparkles: create reset route without CSRF auth
    creating basic routes to initial test without using a CSRF token

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'We tested this successfully!';
+});
+
+Route::get('/health', function () {
+    return response()->json(['status' => 'OK'], 200);
+});
+
+Route::post('/reset', function () {
+    return 'CSRF is disabled for this route.';
 });
